94d27d69ebab0c4f2abd5894446c58a973fc5d93
franklai <franklai@cmlab.csie.ntu.edu.tw>
2009-05-11 22:51:07 +0800

    tudou support!!

--- curl.php
+++ curl.php
@@ -7,7 +7,8 @@ class Curl
 {
 	private $response = NULL;
 
-	public function __construct($url, $data=NULL, $headers=NULL) {
+	public function __construct($url, $data=NULL, $headers=NULL)
+	{
 		$method = 'get';
 
 		if (empty($url)) {
@@ -58,39 +59,66 @@ class Curl
 	{
 		return $this->response->headers;
 	}
+
+	public function get_header($key)
+	{
+		return $this->response->headers[$key];
+	}
 }
 
-class CurlResponse {
+class CurlResponse
+{
+	public $body = '';
+	public $headers = array();
 
-    public $body = '';
-    public $headers = array();
+	public function __construct($response)
+	{
+		# Extract headers from response
+		$pattern = '#HTTP/\d\.\d.*?$.*?\r\n\r\n#ims';
+		preg_match_all($pattern, $response, $matches);
+		$headers = split("\r\n", str_replace("\r\n\r\n", '', array_pop($matches[0])));
+		
+		# Extract the version and status from the first header
+		$version_and_status = array_shift($headers);
+		preg_match('#HTTP/(\d\.\d)\s(\d\d\d)\s(.*)#', $version_and_status, $matches);
+		$this->headers['Http-Version'] = $matches[1];
+		$this->headers['Status-Code'] = $matches[2];
+		$this->headers['Status'] = $matches[2].' '.$matches[3];
+		
+		# Convert headers into an associative array
+		foreach ($headers as $header) {
+			preg_match('#(.*?)\:\s(.*)#', $header, $matches);
+			$this->headers[$matches[1]] = $matches[2];
+		}
+		
+		# Remove the headers from the response body
+		$this->body = preg_replace($pattern, '', $response);
+	}
 
-    public function __construct($response) {
-        # Extract headers from response
-        $pattern = '#HTTP/\d\.\d.*?$.*?\r\n\r\n#ims';
-        preg_match_all($pattern, $response, $matches);
-        $headers = split("\r\n", str_replace("\r\n\r\n", '', array_pop($matches[0])));
-        
-        # Extract the version and status from the first header
-        $version_and_status = array_shift($headers);
-        preg_match('#HTTP/(\d\.\d)\s(\d\d\d)\s(.*)#', $version_and_status, $matches);
-        $this->headers['Http-Version'] = $matches[1];
-        $this->headers['Status-Code'] = $matches[2];
-        $this->headers['Status'] = $matches[2].' '.$matches[3];
-        
-        # Convert headers into an associative array
-        foreach ($headers as $header) {
-            preg_match('#(.*?)\:\s(.*)#', $header, $matches);
-            $this->headers[$matches[1]] = $matches[2];
-        }
-        
-        # Remove the headers from the response body
-        $this->body = preg_replace($pattern, '', $response);
-    }
+	public function __toString()
+	{
+		return $this->body;
+	}
 
-    public function __toString() {
-        return $this->body;
-    }
+	public function get_content()
+	{
+		return $this->body;
+	}
+
+	public function get_headers()
+	{
+		return $this->headers;
+	}
+
+	public function get_header($key)
+	{
+		if (isset($this->headers[$key])) {
+			return $this->headers[$key];
+		} else {
+			// throw exception?
+			return '';
+		}
+	}
 }
 ?>
 
