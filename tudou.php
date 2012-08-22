ee9aa16707be7c2a7f465f6d53b6e9b7ac7acd5e
franklai <franklai@localhost>
2012-08-22 12:16:45 +0800

    fix tudou
    this version only supports url with "programs/view"

--- tudou.php
+++ tudou.php
@@ -1,6 +1,40 @@
 <?php
 require_once('curl.php');
 
+if (!class_exists('Common')) {
+class Common {
+    // return substring that match prefix and suffix
+    // returned string contains prefix and suffix
+    static function getSubString($string, $prefix, $suffix) {
+        $start = strpos($string, $prefix);
+        if ($start === FALSE) {
+            echo "cannot find prefix, string:[$string], prefix[$prefix]\n";
+            return $string;
+        }
+
+        $end = strpos($string, $suffix, $start);
+        if ($end === FALSE) {
+            echo "cannot find suffix\n";
+            return $string;
+        }
+
+        if ($start >= $end) {
+            return $string;
+        }
+
+        return substr($string, $start, $end - $start + strlen($suffix));
+    }
+
+    static function getFirstMatch($string, $pattern) {
+        if (1 === preg_match($pattern, $string, $matches)) {
+            return $matches[1];
+        }
+        return FALSE;
+    }
+}
+}
+
+
 /**
  *1.http://www.tudou.com/programs/view/Veq0WIbqwa8/
  *	取得其 html ，讀出「embed src="http://www.tudou.com/v/Veq0WIbqwa8"」
@@ -18,69 +52,96 @@ class Tudou
 {
 	private $links = array();
 	private $title = 'Unknown Title';
+	private $result = array();
+	private $brtList = array(
+		"2" => "256P",
+		"3" => "360P",
+		"4" => "480P",
+		"5" => "720P",
+		"99"=> "Original"
+	);
 
 	public function __construct($url) {
 		// compare, 
 		// http://www.tudou.com/programs/view/Veq0WIbqwa8/
-		$prefix = 'http://www.tudou.com/programs/view/';
-		if (0 > strncasecmp($url, $prefix, strlen($prefix))) {
-			// throw exception
-			return;
-		}
 
-		// get html of $tudou_url
 		$response = new Curl($url);
 		$html = $response->get_content();
 
-		$pattern = '/embed src="([^"]+)"/';
-		preg_match($pattern, $html, $matches);
-		if (2 != count($matches)) {
-			// throw exception
+
+		$pattern = '/iid = ([0-9]+)/';
+		$iid = Common::getFirstMatch($html, $pattern);
+		if (FALSE === $iid) {
 			return;
 		}
-		$view_url = $matches[1];
 
-		$curl = new Curl($view_url);
-		$location = $curl->get_header('Location');
+		$videoListUrl = sprintf('http://v2.tudou.com/v.action?st=2,3,4,5,99&it=%s', $iid);
 
-		$pattern = '/iid=([0-9]+)/';
-		preg_match($pattern, $location, $matches);
-		if (2 != count($matches)) {
-			// throw exception
-			return;
-		}
-		$iid = $matches[1];
-
-		$cdn_url = sprintf('http://v2.tudou.com/v2/cdn?id=%s', $iid);
-
-		$response = new Curl($cdn_url, NULL, 
-				array('User-Agent' => $_SERVER['HTTP_USER_AGENT']));
+		$response = new Curl($videoListUrl);
 		$xml = $response->get_content();
 
-		$pattern = '/>([^<]+)</';
-		preg_match_all($pattern, $xml, $matches);
-		foreach ($matches[1] as $item) {
-			$link = $item;
-			$this->links[] = $link;
-		}
-
-		// parse title
-		$this->parse_title($html);
+		$videoList = $this->parseXml($xml);
 	}
 
-	private function parse_title($html)
+	private function parseXml($xml)
 	{
-		// don't forget GBK! encoding issue
-		$html = mb_convert_encoding($html, 'UTF-8', 'GBK');
-		
+		$list = array();
 
-		$pattern = '/,kw = "([^"]+)"/';
-		preg_match($pattern, $html, $matches);
-		if (2 != count($matches)) {
-			// error handling
-			return;
+		$decodedXml = htmlspecialchars_decode($xml);
+
+		$title = $this->parse_title($decodedXml);
+
+// 		echo $decodedXml;
+// 		echo str_replace(">", ">\n", $decodedXml);
+
+		$pattern = '/brt="([0-9]+)">(http:[^<]+)</';
+		$ret = preg_match_all($pattern, $decodedXml, $matches, PREG_SET_ORDER);
+
+		if (FALSE === $ret) {
+			return $list;
 		}
-		$this->title = $matches[1];
+
+		if ($ret > 0) {
+			foreach($matches as $match) {
+				$brt = $match[1];
+				$videoUrl = $match[2];
+				$this->result[] = array(
+					"title" => $this->getConvertedTitle($title, $brt),
+					"link" => $videoUrl
+				);
+			}
+		}
+	}
+
+	private function parse_title($xml)
+	{
+		$title = 'Unknown title';
+		$pattern = '/title="([^"]+)"/';
+
+		$ret = Common::getFirstMatch($xml, $pattern);
+		if (FALSE === $ret) {
+			return $title;
+		}
+
+		$title = $ret;
+
+		return $title;
+	}
+
+	private function getConvertedTitle($title, $brt)
+	{
+		$ret = $title . $this->getFormatString($brt);
+
+		return $ret;
+	}
+
+	private function getFormatString($brt)
+	{
+		if (array_key_exists($brt, $this->brtList)) {
+			return sprintf(" (%s)", $this->brtList[$brt]);
+		} else {
+			return sprintf(" (Unknown brt %s)", $brt);
+		}
 	}
 
 	public function get_title()
@@ -91,6 +152,36 @@ class Tudou
 	public function get_links()
 	{
 		return $this->links;
+	}
+
+	public function get_result()
+	{
+		return $this->result;
+	}
+}
+
+if (basename($argv[0]) === basename(__FILE__)) {
+// 	$url = 'http://www.tudou.com/programs/view/YwGhpN2C_KI/';
+// 	$url = 'http://www.tudou.com/programs/view/ogyN7FvbENM/?fr=rec1';
+// 	$url = 'http://www.tudou.com/listplay/GbbmprNY16w/WnMfkxSleL4.html';
+// 	$url = 'http://www.tudou.com/programs/view/UHcWm65Nt-c/';
+	$url = 'http://www.tudou.com/programs/view/7dWtoMMsaYE/';
+// 	$url = 'http://www.tudou.com/programs/view/Xohot2RSQJw/';
+
+	$tudou = new Tudou($url);
+
+	$result = $tudou->get_result();
+
+	$count = count($result);
+
+	echo "count is $count\n";
+
+	for ($idx = 0; $idx < $count; $idx++) {
+		$title = $result[$idx]['title'];
+		$link = $result[$idx]['link'];
+
+		printf("\n%s:\n", $title);
+		printf("\t%s\n", substr($link, 0, strpos($link, "?")));
 	}
 }
 ?>
